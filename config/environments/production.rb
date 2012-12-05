70b83e5fd0bef0fcb24f4d5310c25fefdd2ae92a
Ever <g.everax@gmail.com>
2012-12-05 12:31:48 +0800

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,7 @@ CodeShare::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.initialize_on_precompile = false
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
