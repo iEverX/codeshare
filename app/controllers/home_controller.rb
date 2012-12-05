5f0f79d0f7c6bc1420ea1c40c0914770dd31e9fc
Ever <g.everax@gmail.com>
2012-12-05 12:26:39 +0800

    order->group

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,9 @@
 class HomeController < ApplicationController
   def index
-    snippets = Snippet.select("code_id, max(id)").order('id desc').group(:code_id).limit(10)
-    # snippets = 
-    # snippets = ActiveRecord::Base.connection.execute('select distinct(code_id), id from snippets order by id desc limit 10;')
-
-    puts "+++++++++++++", snippets
+    snippets = Snippet.select("code_id, max(id)").group("code_id").limit(10)
 
     @latest = []
-    snippets.each do |snippet| 
+    snippets.reverse_each do |snippet| 
       @latest.append Snippet.where(:code_id=>snippet[:code_id]).order('id desc').last
     end
 
@@ -22,19 +18,15 @@ class HomeController < ApplicationController
 
   def list
     # snippets = Snippet.order('created_at desc').select('code_id').uniq
-    snippets = Snippet.select("code_id, max(created_at)").order("created_at desc").group(:code_id)
+    snippets = Snippet.select("code_id, max(created_at)").group(:code_id)
 
     @codes = []
-    snippets.each do |snippet|
+    snippets.reverse_each do |snippet|
       code = []
       code.append(Snippet.where(:code_id=>snippet[:code_id]).last)
       code.append(Code.where(:id=>snippet[:code_id]).first)
       @codes.append(code)
     end
-
-    puts "kkkkkkkkkkkkkkkkkkkkkkkk", @codes, "DDDDDDDDDDDDDDDDDDDDDDDDDDDDd"
-    puts @codes[0][0][:description]
-
   end
 
   def show
